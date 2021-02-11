5463693ce227e1ccfe705aa4c487ea07d0a8a14b
HarshadaMulye <Harshada.mulye@iths.se>
2021-02-11 13:31:00 +0100

    Meny added

---
+++ airbean-shop/src/views/KaffeMeny.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <h1>Meny</h1>
+      <Meny/>
+  </div>
+</template>
+
+<script>
+
+import Meny from '@/components/Meny.vue'
+export default {
+  components: { Meny },
+
+}
+</script>
+
+<style>
+
+</style>
