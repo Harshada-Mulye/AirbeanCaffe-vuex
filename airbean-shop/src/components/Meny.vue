995da29ac491c008b43a84cbb39e6bcbd862d3a2
HarshadaMulye <Harshada.mulye@iths.se>
2021-02-12 12:29:19 +0100

    menu file code added

--- airbean-shop/src/components/Meny.vue
+++ airbean-shop/src/components/Meny.vue
@@ -1,12 +1,28 @@
 <template>
   <div>
       <h1>Meny component</h1>
+       <div v-for ="k in kaffe" :key=k.id  class="kaffestyle">
+          <h2>
+              {{k.name}}
+
+          </h2>
+          <h3>
+              {{k.price}}
+          </h3>
+      
+      </div>
+    
   </div>
 </template>
 
 <script>
 export default {
-
+computed:{
+        kaffe()
+        {
+            return this.$store.getters.kaffe;
+        },
+}
 }
 </script>
 
--- airbean-shop/src/store/index.js
+++ airbean-shop/src/store/index.js
@@ -5,6 +5,49 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    kaffe:[
+      {
+      id:1,
+      name:"BryggKaffe",
+      price:49,
+      desc:"Bryggd på månadens bönor"
+      },
+      {
+        id:2,
+        name:"Caffe Dopio",
+        price:49,
+        desc:"Bryggd på månadens bönor"
+        },
+      {
+          id:3,
+          name:"Cappucino",
+          price:49,
+          desc:"Bryggd på månadens bönor"
+          },
+          {
+            id:4,
+            name:"Latte Machiato ",
+            price:49,
+            desc:"Bryggd på månadens bönor"
+            },
+            {
+              id:5,
+              name:"Kaffe LAtte ",
+              price:49,
+              desc:"Bryggd på månadens bönor"
+              },   
+              {
+                id:6,
+                name:"Cortado ",
+                price:39,
+                desc:"Bryggd på månadens bönor"
+                }
+    ]
+  },
+  getters:{
+    kaffe:state=>{
+    return state.kaffe;
+    }
   },
   mutations: {
   },
@@ -12,4 +55,4 @@ export default new Vuex.Store({
   },
   modules: {
   }
-})
+  })
