59b04bf73210d97d73568d81d0b3810256a16444
sofiaMEweiss <sofia.weiss@iths.se>
2021-02-17 15:22:07 +0100

    Home page added HTML & CSS

--- airbean-shop/src/views/Home.vue
+++ airbean-shop/src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <img class="left" src="../assets/homeleft.svg" />
+    <img src="../assets/homelogo.svg" />
+    <img class="right" src="../assets/homeright.svg" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  // components: {
+  //   HelloWorld
+  // }
+};
 </script>
+
+<style scoped>
+.home {
+  display: flex;
+  justify-content: center;
+  min-height: 500px;
+  background-color: #0e927d;
+  max-width: 375px;
+}
+
+.left {
+  margin-right: -97px;
+}
+
+.right {
+  margin-left: -97px;
+}
+</style>
