c3be4e46ad1121cd535c222513c3d0dffb0aca30
sofiaMEweiss <sofia.weiss@iths.se>
2021-02-17 12:14:29 +0100

    Order delivery page CSS + HTML

--- airbean-shop/src/views/OrderDelivery.vue
+++ airbean-shop/src/views/OrderDelivery.vue
@@ -1,10 +1,58 @@
 <template>
-  <div>OrderDelivery</div>
+  <section>
+    <span>Ordernummer #</span>
+    <img src="../assets/orderdelivery.svg" />
+    <div>
+      <h1>Din beställning är på väg!</h1>
+      <span> minuter</span>
+    </div>
+    <button>Ok, cool!</button>
+  </section>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style scoped>
+section {
+  background-color: #e5674e;
+  color: white;
+  padding: 50px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  min-height: 500px;
+  align-items: center;
+  font-family: sans-serif;
+}
+
+span {
+  letter-spacing: 0.07rem;
+}
+span:first-child {
+  color: #f7d1ca;
+}
+
+h1 {
+  font-family: serif;
+  margin-top: 0;
+  font-size: 2.3rem;
+  line-height: 120%;
+}
+
+span:last-child {
+  font-size: 1.2rem;
+}
+
+button {
+  border-radius: 50px;
+  padding: 15px 50px;
+  font-family: serif;
+  font-weight: bold;
+  font-size: 1.6rem;
+  cursor: pointer;
+  outline: none;
+  border: none;
+}
 </style>
