c1e685752632ceaafd3093f7ac9065943945de73
sofiaMEweiss <sofia.weiss@iths.se>
2021-02-18 09:15:37 +0100

    Started with nav

--- airbean-shop/src/router/index.js
+++ airbean-shop/src/router/index.js
@@ -35,6 +35,12 @@ const routes = [
     name: 'OrderDelivery',
 
     component: () => import(/* webpackChunkName: "about" */ '../views/OrderDelivery.vue')
+  },
+    {
+    path: '/NavDropDown',
+    name: 'NavDropDown',
+
+    component: () => import(/* webpackChunkName: "about" */ '../views/NavDropDown.vue')
   }
 ]
 
---
+++ airbean-shop/src/views/NavDropDown.vue
@@ -0,0 +1,10 @@
+<template>
+  <nav>
+    <ul>
+    <li>Meny</li>
+    <li><router-link to="/OurCoffee">Vårt Kaffe</router-link>|</li>
+    <li>Min profil</li>
+    <li>Orderstatus</li>
+    </ul>
+  </nav>
+</template>
