8b80fc867da67d88be9787ceebc1814eef8d7620
sofiaMEweiss <sofia.weiss@iths.se>
2021-02-18 22:25:13 +0100

    my profile page almost finished

--- airbean-shop/src/components/MinProfil.vue
+++ airbean-shop/src/components/MinProfil.vue
@@ -1,20 +1,25 @@
 <template>
   <section class="my-profil-background">
     <img src="../assets/ourcoffeetop.svg" />
-    <section class="login">
+    <div class="login-container">
+      <img src="../assets/myprofilelogo.svg" />
       <h1>Välkommen till AirBean-familjen!</h1>
       <p>
         Genom att skapa ett konto nedan kan du spara och se din orderhistorik.
       </p>
       <form @submit.prevent="onSubmit(profile)">
         <label for="name">Name</label>
-        <input type="text" v-model="profile.name" name="name" /><br />
+        <input type="text" v-model="profile.name" name="name" />
         <label for="email">Email</label>
-        <input type="text" v-model="profile.email" name="email" /><br />
+        <input type="text" v-model="profile.email" name="email" />
+        <label>
+          <input class="radio-button" type="radio" name="radio" />
+          GDPR OK!
+        </label>
 
         <input type="submit" value="Brew Me a Cup" />
       </form>
-    </section>
+    </div>
   </section>
 </template>
 
@@ -52,14 +57,33 @@ export default {
   align-items: center;
   height: 100%;
 }
-.login {
+.login-container {
   background-color: #f3e4e1;
   margin: -35px 15px 15px 15px;
-  padding: 20px;
+  padding: 35px 20px;
   display: flex;
   flex-direction: column;
-  align-content: space-between;
+  justify-content: space-around;
   height: 100%;
+  font-family: sans-serif;
+}
+
+div > img {
+  width: 50px;
+  align-self: center;
+}
+
+h1 {
+  font-family: serif;
+  margin: 0 0 15px 0;
+  font-size: 2.3rem;
+  line-height: 100%;
+}
+p {
+  margin: 0 0 50px 0;
+  letter-spacing: 0.07rem;
+  font-size: 0.9rem;
+  line-height: 150%;
 }
 
 form {
@@ -67,6 +91,25 @@ form {
   flex-direction: column;
   align-content: space-between;
   text-align: left;
+}
+
+label {
+  font-size: 0.7rem;
+  margin-bottom: 3px;
+  font-family: sans-serif;
+}
+
+input {
+  padding: 9px;
+  border-radius: 5px;
+  border: #2f2926 solid 1px;
+  font-size: 0.9rem;
+  background-color: #f3e4e1;
+  margin-bottom: 20px;
+}
+
+.radio-button {
+  margin-bottom: 50px;
 }
 
 input:last-of-type {
